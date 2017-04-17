a5c4258c00b32682e9434aa666ac9e79be122b68
UCNK FF UK <UCNK FF UK>
2017-04-17 21:23:06 +0200

    Divocarna s uhly

---
+++ 2domecek.py
@@ -0,0 +1,28 @@
+from turtle import forward, left, right, exitonclick
+from math import degrees, atan, sqrt, asin
+
+def domecek(a, b):
+
+    forward(a)
+    left(90)
+    forward(b)
+    left(180-degrees(atan(a/b)))
+    forward(sqrt(a**2 + b**2))
+    left(90 + degrees(atan(a/b)))
+    forward(a)
+    left(180-degrees(atan(b/a)))
+    forward(sqrt(a**2 + b**2))
+    left(90 + degrees(atan(b/a)))
+    forward(b)
+    left(180)
+    forward(b)
+    right(90)
+    forward(a)
+    left(180-degrees(atan(b/a)))
+    forward((sqrt(a**2 + b**2)/2))
+    left(180-degrees(2*(asin((a/2)/(sqrt(a**2 + b**2)/2)))))
+    forward((sqrt(a**2 + b**2)/2))
+
+    exitonclick()
+
+domecek(100, 100)
